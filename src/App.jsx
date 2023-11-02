4b7f7b9732fd8ca36d446f4347b36a4159e15191
muohannedx4 <mohanned.kadache@ensia.edu.dz>
2023-11-02 19:20:47 +0100

    setting up the environment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import supabase from "./config/supabaseClient";
+
+function App() {
+
+  console.log(supabase)
+
+  return (
+    <div>
+      Helloooo friends
+    </div>
+  );
+}
+
+export default App;
