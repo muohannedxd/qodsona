6b63e14bef3521580bb32cb8d170cc186ccb49bb
muohannedx4 <mohanned.kadache@ensia.edu.dz>
2023-11-03 09:37:46 +0100

    navbar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import { useEffect, useState } from 'react';
-import { DocumentData, collection, getDocs, onSnapshot } from 'firebase/firestore';
-import db from "./config/firebase"
+// import { useEffect, useState } from 'react';
+// import { DocumentData, collection, getDocs, onSnapshot } from 'firebase/firestore';
+// import db from "./config/firebase"
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Post from "./pages/Post";
+import Search from "./pages/Search";
+import Login from "./pages/Login";
 
 function App() {
-
-  const [users, setUsers] = useState<DocumentData[]>([])
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const querySnapshot = await getDocs(collection(db, 'users'))
-      const users = querySnapshot.docs.map(doc => doc.data())
-      setUsers(users)
-    }
-
-    getUsers()
-
-  }, [])
+  //const [users, setUsers] = useState<DocumentData[]>([])
+  //
+  //useEffect(() => {
+  //  const getUsers = async () => {
+  //    const querySnapshot = await getDocs(collection(db, 'users'))
+  //    const users = querySnapshot.docs.map(doc => doc.data())
+  //    setUsers(users)
+  //  }
+  //
+  //  getUsers()
+  //
+  //}, [])
 
   return (
-    <div className='mx-48 h-screen'>
-      {users.map(user => {
-        return (
-          <div>
-            <p> {user.name} </p>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />}></Route>
+        <Route path="/post" element={<Post />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
+import logo from "../assets/images/icon.png"
+import { Button } from "./ui/button"
 
 export default function Navbar() {
   return (
-    <div>
-      
+    <div className='flex items-center justify-between py-4 md:py-8 lg:py-16'>
+      <img src={logo} alt="logo" />
+      <div className="flex items-center gap-4 md:gap-8 lg:gap-12 xl:gap-16">
+        <NavLink to='/post'> Post </NavLink>
+        <NavLink to='/search'> Search </NavLink>
+        <NavLink to='/'> About Us </NavLink>
+        <NavLink to='/login'> Login </NavLink>
+      </div>
     </div>
   )
 }
