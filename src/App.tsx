319283aee3993c827534a87a49db7df609e2ea66
muohannedx4 <mohanned.kadache@ensia.edu.dz>
2023-11-03 01:13:10 +0100

    setting up the environment

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,52 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+
 function App() {
+  const items = [
+    {
+      id: 1,
+      trigger: "Is is accessible?",
+      content: "Yes. It adheres to the WAI-ARIA design pattern.",
+    },
+    {
+      id: 2,
+      trigger: "Is it styled?",
+      content: `Yes. It comes with default styles that matches the other
+      components aesthetic.`,
+    },
+    {
+      id: 3,
+      trigger: "Is it animated?",
+      content: `Yes. It's animated by default, but you can disable it if you
+      prefer.`,
+    },
+  ];
+
   return (
-    <>
-      <p>Hello ya mohanned</p>
-      <div className="mx-48">
+    <div className="mx-48">
+      <p>BOUBA CHIKOUR</p>
+      <div>
         <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Is it styled?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components&apos; aesthetic.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Is it animated?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It&apos;s animated by default, but you can disable it if you
-              prefer.
-            </AccordionContent>
-          </AccordionItem>
+          {items.map((item) => {
+            return (
+              <AccordionItem key={item.id} value={`item-${item.id}`}>
+                <AccordionTrigger> {item.trigger} </AccordionTrigger>
+                <AccordionContent>
+                  {item.content}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
         </Accordion>
       </div>
-    </>
+      <div className="flex items-center space-x-2">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      </div>
+    </div>
   );
 }
 
