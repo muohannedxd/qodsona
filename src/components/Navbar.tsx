eb7631c51d465b2f81c8412d969a23798f4f2461
AbderrahimRezki <abderrahim4cem@gmail.com>
2023-11-03 14:36:39 +0100

    added navabar hbmenu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,68 @@
 import { NavLink, Link } from "react-router-dom"
 import logo from "../assets/images/icon.png"
-import { Button } from "./ui/button"
+import { AlignLeft } from "lucide-react";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
+interface HamburgerMenuProps {
+  navLinks: Object,
+}
+
+const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ navLinks }) =>
+  <Accordion type="single" collapsible>
+    <AccordionItem value='hb-menu'>
+      <AccordionTrigger>
+        <AlignLeft />
+      </AccordionTrigger>
+      <AccordionContent className="absolute">
+        <div className="flex flex-col gap-1">
+          {
+            Object.entries(navLinks).map(
+              ([label, link], index) =>
+                <div className="px-5 py-2 bg-white rounded-[8px]">
+                  <Link to={link} key={`dd-link-${index}`} className="hover:bg-[#ffffff]">{label}</Link>
+                </div>
+            )
+          }
+        </div>
+      </AccordionContent>
+
+    </AccordionItem>
+  </Accordion>
+
 
 export default function Navbar() {
+
+  const navLinks = {
+    "Post": "/post",
+    "Search": "/search",
+    "About Us": "/about",
+    "Login": "/login"
+  }
+
   return (
     <div className='flex items-center justify-between py-4 md:py-8 lg:py-16'>
       <Link to='/'>
         <img src={logo} alt="logo" />
       </Link>
-      <div className="flex items-center gap-4 md:gap-8 lg:gap-12 xl:gap-16">
-        <NavLink to='/post'> Post </NavLink>
-        <NavLink to='/search'> Search </NavLink>
-        <NavLink to='/about'> About Us </NavLink>
-        <NavLink to='/login'> Login </NavLink>
+
+      <div className="md:hidden px-10">
+        <HamburgerMenu navLinks={navLinks} />
+      </div>
+
+      <div className="hidden md:flex items-center gap-4 md:gap-8 lg:gap-12 xl:gap-16">
+        {
+          Object.entries(navLinks).map(
+            ([label, link], index) =>
+              <NavLink key={`nav-item-${index}`} to={link}> {label} </NavLink>
+          )
+        }
       </div>
     </div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button"
+ 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className='text-center flex flex-col gap-20 items-center px-10 pt-20'>
+      <div>
+        <h1 className="font-bold 
+          text-3xl
+          md:text-5xl">
+                Lorem ipsum Big Title</h1>
+        <h2 className="font-semibold md:text-3xl">Lorem ipsum subtitle</h2>
+      </div>
+      
+      <p className="
+      text-[1.15rem]
+      md:max-w-[80%] xl:max-w-[50%]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+      <div>
+        <Button className="mx-3 md:text-5xl font-extrabold h-auto border-black border-4" asChild>
+          <Link to="/search">Search</Link>
+        </Button>
+        <Button className="mx-3 md:text-5xl font-extrabold h-auto bg-transparent border-black border-4" variant="outline">
+          <Link to="/post">Post</Link>
+        </Button>
+      </div>
     </div>
   )
 }
