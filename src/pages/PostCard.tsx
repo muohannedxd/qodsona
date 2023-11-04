94bad666532f3531ae8ef570b11e286581ecb3ab
KorichiAyoub <124984361+KorichiAyoub@users.noreply.github.com>
2023-11-04 23:55:36 +0100

    Update PostCard.tsx

--- src/pages/PostCard.tsx
+++ src/pages/PostCard.tsx
@@ -1,68 +1,46 @@
-<<<<<<< HEAD
-import Tag from "./tag";
-
-const PostCard = () => {
-
-    return (
-
-        <div className="card" style={{ margin: '20px' }}>
-          <div className="card-body">
-            <h3 className="card-title">Special title treatment</h3>
-            <div className="row">
-        <div className="col d-flex align-items-center">
-        <p>Tags:</p>
-      </div>
-      <div className="col d-flex align-items-center">
-        <Tag value="hello" />
-        <Tag value="hello" />
-        <Tag value="dsgsdhsdh" />
-      </div>
-            </div>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            
-            <div className="text-right"> {/* Add a class to align content to the right */}
-              <a href="#" className="btn btn-primary" style={{ backgroundColor: 'black',marginBottom:'1px' }}>Add location</a>
-              <a href="#" className="btn btn-primary" style={{ backgroundColor: 'black', marginLeft: '10px' }}>call</a>
-            </div>
-          </div>
-        </div>
-
-  );
-  }
-
-export default PostCard;
-=======
 import React from "react";
 import Tag from "./tag";
 
-const PostCard = ({ postType, description, tags }) => {
+const PostCard = ({ postType, description, tags, imageURLs }) => {
+    const defaultImageUrl = 'https://firebasestorage.googleapis.com/v0/b/qodsona-cf756.appspot.com/o/images%2FBeep%20Beep%20-%20Avatar(1).pngk_YS5JIKeNJWPpCjv4qLR?alt=media&token=326cb83d-cde1-407a-8269-7ad9f0200904';
+
     return (
-        <div className="card" style={{ margin: '20px' }}>
-            <div className="card-body">
-                {/* Use title prop */}
-                <h3 className="card-title">{postType || 'Special title treatment'}</h3>
+        <div className="card" style={{ margin: '20px', minWidth: '250px', minHeight: '350px'}}>
+            <div className="card-body d-flex flex-column" style={{ flexGrow: 1 }}>
+                <h3 className="card-title">{postType || 'Post Type'}</h3>
                 <div className="row">
                     <div className="col d-flex align-items-center">
                         <p>Tags:</p>
                     </div>
                     <div className="col d-flex align-items-center">
-                        {/* Map through tags prop */}
                         {tags && tags.map((tagValue, index) => (
                             <Tag key={index} value={tagValue} />
                         ))}
                     </div>
                 </div>
-                {/* Use description prop */}
-                <p className="card-text">{description || 'this is the desc'}</p>
-                
-                <div className="text-right">
-                    <a href="#" className="btn btn-primary" style={{ backgroundColor: 'black', marginBottom: '1px' }}>Add location</a>
-                    <a href="#" className="btn btn-primary" style={{ backgroundColor: 'black', marginLeft: '10px' }}>call</a>
+
+                <div className="grid grid-cols-4 gap-1" style={{ marginTop: '20px' }}>
+                    {imageURLs && imageURLs.length > 0 ? (
+                        imageURLs.map((url, index) => (
+                            <div key={index} className="overflow-hidden max-h-[20rem] max-w-[20rem] rounded-[8px] object-cover">
+                                <img src={url} className="max-h-full w-auto" alt={`Image ${index + 1}`} />
+                            </div>
+                        ))
+                    ) : (
+                        <div className="overflow-hidden max-h-[20rem] max-w-[20rem] rounded-[8px] object-cover">
+                            <img src={defaultImageUrl} className="max-h-full w-auto" alt="Default Image" />
+                        </div>
+                    )}
                 </div>
+
+                <p className="card-text">{description || 'No description provided.'}</p>
+            </div>
+            <div className="text-right" style={{ marginBottom: '10px' }}>
+                <a href="#" className="btn btn-primary" style={{ backgroundColor: 'black', marginRight: '10px', marginBottom: '10px' }}>Add location</a>
+                <a href="#" className="btn btn-primary" style={{ backgroundColor: 'black', marginRight: '10px', marginBottom: '10px' }}>Call</a>
             </div>
         </div>
     );
 }
 
 export default PostCard;
->>>>>>> 87c988a30b21dd44ce6d2c2431b18bf6a5569a1f
