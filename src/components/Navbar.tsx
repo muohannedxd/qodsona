0cef2b4b11f9116449dea2d7b142d531c9ae2621
muohannedx4 <mohanned.kadache@ensia.edu.dz>
2023-11-04 14:58:01 +0100

    profile done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,21 @@
 import { NavLink, Link } from "react-router-dom";
-import logo from "../assets/images/icon.png";
+import logo from "@/assets/images/9ods_logo.png";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useAuth } from "@/auth/useAuth";
-import { useState } from "react";
 
 export default function Navbar() {
   const { isLoggedIn, user } = useAuth();
-  
+
   if (isLoggedIn && user) {
-    var currentEmail = user.email
+    var currentEmail = user.email;
   }
 
   return (
     <div className="flex items-center justify-between py-4 md:py-8 lg:py-16">
       <Link to="/">
-        <img src={logo} alt="logo" />
+        <Avatar>
+          <AvatarImage src={logo} alt="logo" />
+        </Avatar>
       </Link>
       <div className="flex items-center gap-4 md:gap-8 lg:gap-12 xl:gap-16">
         <NavLink to="/post"> Post </NavLink>
@@ -23,7 +24,7 @@ export default function Navbar() {
         {!isLoggedIn ? (
           <NavLink to="/login"> Join </NavLink>
         ) : (
-          <Link to='/profile'>
+          <Link to="/profile">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn" alt="@shadcn" />
               <AvatarFallback> {currentEmail.substring(0, 2)} </AvatarFallback>
